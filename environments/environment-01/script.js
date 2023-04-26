ca7de31a50ce688ea2ba583baea269789c6074eb
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-04-26 13:54:15 +0200

    forsøg på opg 01

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,28 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+let users = [];
+
+async function start() {
+  console.log("js is running");
+
+  users = await getUsers();
+}
+
+async function getUsers() {
+  const response = await fetch("users.json");
+  const data = await response.json();
+
+  console.log(data);
+
+  return data;
+}
+
+// Lav en funktion der viser listen på websiden -
+// hver user skal vises med navn og hvorvidt de er aktive eller ej.
+
+function showUsers(users) {
+  for (const user in users) {
+  }
+}
