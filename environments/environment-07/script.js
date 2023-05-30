667e243b6003de8c2aa510b6e193b9d84193c328
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-05-30 11:40:46 +0200

    prøve eksamen

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,51 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+let students = [];
+
+function start() {
+  // createStudent(name, email, age);
+  document.querySelector("#create-student-form").addEventListener("submit", createStudent);
+
+  //   showStudents(students);
+}
+
+function createStudent(event) {
+  event.preventDefault();
+
+  const form = event.target;
+
+  const newStudent = {
+    name: form.name.value,
+    email: form.email.value,
+    age: Number(form.age.value)
+  };
+
+  students.push(newStudent);
+  console.log(students);
+  showStudents();
+}
+
+function showStudents() {
+  document.querySelector("#students-table-body").innerHTML = "";
+  students.sort((a, b) => a.name.localeCompare(b.name));
+  for (const student of students) {
+    if (student.age >= 18) {
+      showStudent(student);
+    }
+  }
+}
+
+function showStudent(student) {
+  const html = /*html */ `
+        <tr>
+            <td>${student.name}</td>
+            <td>${student.email}</td>
+            <td>${student.age}</td>
+        </tr>
+    
+    `;
+
+  document.querySelector("#students-table-body").insertAdjacentHTML("beforeend", html);
+}
