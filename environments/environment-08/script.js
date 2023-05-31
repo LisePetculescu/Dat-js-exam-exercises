77b2dfb2b8465042423ed950b095858ca5e31d21
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-05-31 11:52:29 +0200

    hihi fuk

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,45 @@
 "use strict";
+window.addEventListener("load", start);
+
+let songList = [];
+
+function start() {
+    console.log("hello");
+
+    getSongs();
+}
+
+async function getSongs() {
+    const response = await fetch("./playlist.json")
+    const data = await response.json()
+
+    songList = data;
+    console.log(songList);
+    showSongs(songList);
+}
+
+function showSongs(songList) {
+    document.querySelector("#songlist").innerHTML = "";
+    for (const song of songList) {
+        console.log(song);
+        showSong(song);
+    }
+}
+
+function showSong(song) {
+    const html = /*html */ ` 
+    
+    <li>${song.artist}, ${song.title}, ${song.duration} <button>Delete</button></li>
+    `
+    document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+        document.querySelector("#songlist li:last-child button").addEventListener("click", () => deleteSong(song));
+
+}
+
+function deleteSong(song) {
+    // console.log(song);
+    const songToRemove = song;
+    songList = songList.filter(song => song.title != songToRemove.title)
+    showSongs(songList);
+
+}
