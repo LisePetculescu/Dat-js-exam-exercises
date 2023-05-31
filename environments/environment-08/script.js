69b1f792f096916059bee9162b8aa734ac4b0bdb
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-05-31 11:12:17 +0200

    hihi

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,48 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+const songList = [];
+
+function start() {
+  console.log("hello");
+
+  // addSong();
+  document.querySelector("#add-song-form").addEventListener("submit", addSong);
+}
+
+function addSong(artist, title, duration) {
+  const newSong = {
+    artist: artist,
+    title: title,
+    duration: duration
+  };
+  songList.push(newSong);
+  console.log(songList);
+}
+
+function addSongClicked(event) {
+  event.preventDefault;
+  newSong = {
+    artist: form.name.value,
+    title: form.title.value,
+    duration: Number(form.duration.value)
+  };
+}
+
+function showSongs() {
+  document.querySelector("#songlist").innerHTML = "";
+
+  for (const song of songList) {
+    showSongs(song);
+  }
+}
+
+function showSong() {
+  const html = /*html */ `
+    <li>${song.artist}, ${song.title}, ${song.duration}</li>
+    
+    `;
+
+  document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+}
