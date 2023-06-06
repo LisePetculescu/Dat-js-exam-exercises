d0b6d70f005132027f4d3cddc76a6c9e5680328d
Lise Freja Jensen Petculescu <lise.lili97@gmail.com>
2023-06-06 15:36:03 +0200

    test 12 in less than 15 min

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,47 @@
 "use strict";
+import { courses } from "./courses.js";
+
+// # Øvelse 12
+
+// Environment: `environment-05`
+
+// 1. Importer `courses`-listen i `script.js`.
+
+window.addEventListener("load", start);
+
+function start() {
+    console.log("hello");
+    console.log(courses);
+
+    sortByEcts();
+    showCourses();
+}
+
+
+// 2. Lav en funktion, der viser listen af alle `course`-objekter på websiden. Vis som minimum `name` og `ectsPoints`.
+
+
+function showCourses() {
+        for (const course of courses) {
+            showCourse(course)
+        }
+
+}
+
+function showCourse(course) {
+    const html = /* html */ `
+    <br>
+    <li>${course.name}</li>
+    <li>ects points: ${course.ectsPoints}</li>
+    `
+
+    document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+}
+
+
+// 3. Lav en funktion, der sorterer listen af `courses` efter stigende antal ECTS. Vis den sorterde liste på websiden.
+
+function sortByEcts() {
+
+    courses.sort((a, b) => a.ectsPoints - b.ectsPoints);
+}
