63cedf459421b3aa93bd64648d611dcf2159c0e7
Lise Freja Jensen Petculescu <lise.lili97@gmail.com>
2023-06-06 17:23:10 +0200

    gg

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -8,21 +8,27 @@ import { courses } from "./courses.js";
 // 1. Importér `courses`-listen i `script.js`.
 window.addEventListener("load", start);
 
+// let newCourseList = courses;
+// console.log("gg", newCourseList);
+
 function start() {
     console.log("hello");
 
     console.log(courses);
 
-document.querySelector("#select-filter-ects").addEventListener("change", filterByEcts)
 
-    showCourses();
+    showCourses(courses);
+ document
+   .querySelector("#select-filter-ects")
+   .addEventListener("change", filterByEcts);
+// filterByEcts();
 }
 
 // 2. Lav en funktion, der viser listen af alle `course`-objekter på websiden. Vis som minimum `name` og `ectsPoints`.
 
-function showCourses() {
-
-    for (const course of courses) {
+function showCourses(filteredCourses) {
+    document.querySelector("#courses-list").innerHTML = "";
+    for (const course of filteredCourses) {
         showCourse(course)
     }
 
@@ -38,17 +44,22 @@ function showCourse(course) {
     `
 
     document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+   
 }
 
 // 3. Lav en funktion, der filtrer listen af `courses` på baggrund af valgte `option` i `select` (se `environment-05`). 
+// Filtreringen ændrer sig hver gang en ny `option` vælges.
 
-function filterByEcts() {
+function filterByEcts(event) {
 
-    const selected = document.querySelector(option).value;
-    if (selected === "5") {
-        courses.filter()
-    }
+    const selected = Number(event.target.value);
+    // if (selected == "5") {
+    //    newCourseList = courses.filter(course => course.ectsPoints == "5")
+    // }
+
+    const newCourseList = courses.filter(course => course.ectsPoints === selected)
+    // eller .toString()
+
+    showCourses(newCourseList);
 }
 
-
-// Filtreringen ændrer sig hver gang en ny `option` vælges.
