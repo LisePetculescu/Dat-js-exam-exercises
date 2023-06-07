cee8b63fcfff967bde636ed50a790887ba149e50
Lise Freja Jensen Petculescu <lise.lili97@gmail.com>
2023-06-07 11:02:22 +0200

    gg

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+const studentList = [];
+
+function start() {
+
+console.log("hello");
+
+document.querySelector("#create-student-form").addEventListener("submit", addStudent);
+
+}
+
+function addStudent(event) {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const newStudent = {
+        name: form.name.value,
+        email: form.email.value,
+        age: form.age.value
+    }
+
+    studentList.push(newStudent);
+    console.log("student objekt", newStudent);
+    console.log("listen", studentList);
+}
