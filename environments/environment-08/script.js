622b4168e4d249b95d7fb42ebfb9c973f689c1fc
Lise Freja Jensen Petculescu <lise.lili97@gmail.com>
2023-06-08 19:43:28 +0200

    gg

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -8,51 +8,54 @@
 
 // 1. Lav en funktion der indlæser JSON-filen `playlist.json` og gemmer listen i en variabel.
 
+let thePlaylist = [];
+
 window.addEventListener("load", start);
 
 async function start() {
-    console.log("Hello");
+  console.log("Hello");
 
-   const thePlaylist = await getPlaylist();
-   showPlaylist(thePlaylist);
+  thePlaylist = await getPlaylist();
+  showPlaylist(thePlaylist);
 }
 
 async function getPlaylist() {
-    const response = await fetch("playlist.json");
-    const data = await response.json();
+  const response = await fetch("playlist.json");
+  const data = await response.json();
 
-    console.log(data);
-    return data;
+  console.log(data);
+  return data;
 }
 
-
-
-// 2. Lav en funktion til at udskrive listen af sange på websiden- 
+// 2. Lav en funktion til at udskrive listen af sange på websiden-
 // tilføj en `remove`-knap til hver sang.
 
 function showPlaylist(thePlaylist) {
-    for (const song of thePlaylist) {
-        showSongs(song)
-    }
+  for (const song of thePlaylist) {
+    showSongs(song);
+  }
 }
 
 function showSongs(song) {
-    const html = /* html */ `
+  const html = /* html */ `
     <br>
-    <li>ARTIST: ${song.artist}, TITLE: ${song.title}, (DURATION) ${song.duration}</li>
-    <button id="removeSong">Delete</button>
+    <li>ARTIST: ${song.artist}, TITLE: ${song.title}, (DURATION) ${song.duration} <button id="removeSong">Delete</button></li>
+    
     `;
 
-    document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+  document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
 
-    document.querySelector("#songlist li:last-child").addEventListener("click", removeChosenSong);
+  document
+    .querySelector("#songlist li:last-child button")
+    .addEventListener("click", () => removeChosenSong(song));
 }
-
-
 
 // 3. Få remove-knappen til at fjerne den pågældende sang fra listen, og udskriv listen igen.
 
-function removeChosenSong(event) {
+function removeChosenSong(song) {
+  console.log(song.title);
+  const songToRemove = song;
 
-    console.log(event.target);
+  thePlaylist = thePlaylist.filter(song => song.title != songToRemove.title);
+  showPlaylist(thePlaylist);
 }
