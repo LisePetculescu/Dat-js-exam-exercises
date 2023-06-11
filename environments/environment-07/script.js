9243fbdbb216bff51234a90e1359d9bb45646990
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-06-11 19:15:26 +0200

    gg

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -6,7 +6,7 @@
 
 // *Prøveeksamen #3*
 
-// 1. Lav en funktion der opretter et `student` objekt med `name`, `email` og `age`, 
+// 1. Lav en funktion der opretter et `student` objekt med `name`, `email` og `age`,
 // fra formularen på websiden og tilføjer det til en liste.
 
 window.addEventListener("load", start);
@@ -14,46 +14,44 @@ window.addEventListener("load", start);
 let studentList = [];
 
 function start() {
-    console.log("Hello");
+  console.log("Hello");
 
-    document.querySelector("#create-student-form").addEventListener("submit", createStudent);
+  document.querySelector("#create-student-form").addEventListener("submit", createStudent);
 }
 
 function createStudent(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const form = event.target;
+  const form = event.target;
 
-    const newStudent = {
-        name: form.name.value,
-        email: form.email.value,
-        age: Number(form.age.value)
-    }
+  const newStudent = {
+    name: form.name.value,
+    email: form.email.value,
+    age: Number(form.age.value)
+  };
 
-
-    studentList.push(newStudent)
-    console.log(studentList);
-    sortByAge();
-    showAdultStudents();
+  studentList.push(newStudent);
+  console.log(studentList);
+  //   sortByName();
+  sortByAge();
+  showAdultStudents();
 }
-
 
 // 2. Lav en anden funktion til at vise listen på websiden,
 // men undlad eventuelle students der er under 18 år.
 
 function showAdultStudents() {
-    document.querySelector("#students-table-body").innerHTML = "";
+  document.querySelector("#students-table-body").innerHTML = "";
 
-    for (const student of studentList) {
-        if (student.age >= 18) {
-            showTheStudents(student)
-        }
+  for (const student of studentList) {
+    if (student.age >= 18) {
+      showTheStudents(student);
     }
-
+  }
 }
 
 function showTheStudents(student) {
-    const html = /* html */ `
+  const html = /* html */ `
      <tr>
             <td>${student.name}</td>
             <td>${student.email}</td>
@@ -61,14 +59,15 @@ function showTheStudents(student) {
           </tr>
     `;
 
-    document.querySelector("#students-table-body").insertAdjacentHTML("beforeend", html);
-    
+  document.querySelector("#students-table-body").insertAdjacentHTML("beforeend", html);
 }
-
-
 
 // 3. Sortér listen alfabetisk efter `name`.
 
+function sortByName() {
+  studentList.sort((studentA, studentB) => studentA.name.localeCompare(studentB.name));
+}
+
 function sortByAge() {
-    studentList = studentList.sort((studentA, studentB) => studentA.name.localeCompare(studentB.name));
+  studentList.sort((studentA, studentB) => studentA.age - studentB.age);
 }
