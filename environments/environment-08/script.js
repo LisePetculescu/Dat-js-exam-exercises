6c839373e7022139fe6012df91c8b3f3488a4550
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-06-11 19:03:21 +0200

    gg

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -6,7 +6,7 @@
 
 // *Prøveeksamen #5 (sæt 2)*
 
-// 1. Opret en liste af sang-objekter, hvor hver sang indeholder `artist`, `title` 
+// 1. Opret en liste af sang-objekter, hvor hver sang indeholder `artist`, `title`
 // og `duration` (i form af minutter:sekunder, fx 3:21)
 
 window.addEventListener("load", start);
@@ -18,51 +18,42 @@ const songList = [
 ];
 
 function start() {
+  console.log("Hello");
 
-    console.log("Hello");
+  console.log(songList);
 
-    console.log(songList);
-
-    loopSongs();
-
+  loopSongs();
 }
 
-
-// 2. Lav en funktion til at udskrive listen af sange på websiden - 
+// 2. Lav en funktion til at udskrive listen af sange på websiden -
 // tilføj en `upvote`-knap til hver sang.
 
 function loopSongs() {
-    document.querySelector("#songlist").innerHTML = "";
+  document.querySelector("#songlist").innerHTML = "";
 
-    for (const song of songList) {
-        showSong(song)
-    }
+  for (const song of songList) {
+    showSong(song);
+  }
 }
 
 function showSong(song) {
-    const html = /* html */ `
+  const html = /* html */ `
     <br>
     <li>ARTIST: ${song.artist} TITLE: ${song.title} (DURATION) ${song.duration} votes: ${song.votes} <button>Upvote</button> </li>
     `;
 
-    document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+  document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
 
-    document.querySelector("#songlist li:last-child button").addEventListener("click", () => upvoteClicked(song))
+  document.querySelector("#songlist li:last-child button").addEventListener("click", () => upvoteClicked(song));
 }
 
-
-
-// 3. Få `upvote`-knappen til flytte den pågældende sang én tak op i listen 
+// 3. Få `upvote`-knappen til flytte den pågældende sang én tak op i listen
 // - udskriv derefter listen igen.
 
 function upvoteClicked(song) {
+  song.votes++;
+  console.log(song);
+  songList.sort((songA, songB) => songB.votes - songA.votes);
 
-song.votes++;
-console.log(song);
-songList.sort((songA, songB) => songB.votes - songA.votes)
-
-loopSongs();
-
-
-
+  loopSongs();
 }
