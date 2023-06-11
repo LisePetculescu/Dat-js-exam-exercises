ea9a5a0a0c8bcd8ddc83db505c5e84b53d63cc2a
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-06-11 19:53:34 +0200

    gg

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -12,54 +12,40 @@ window.addEventListener("load", start);
 // console.log("gg", newCourseList);
 
 function start() {
-    console.log("hello");
+  console.log("hello");
 
-    console.log(courses);
+  console.log(courses);
 
-
-    showCourses(courses);
- document
-   .querySelector("#select-filter-ects")
-   .addEventListener("change", filterByEcts);
-// filterByEcts();
+  showCourses(courses);
+  document.querySelector("#select-filter-ects").addEventListener("change", filterByEcts);
+  // filterByEcts();
 }
 
 // 2. Lav en funktion, der viser listen af alle `course`-objekter på websiden. Vis som minimum `name` og `ectsPoints`.
 
 function showCourses(filteredCourses) {
-    document.querySelector("#courses-list").innerHTML = "";
-    for (const course of filteredCourses) {
-        showCourse(course)
-    }
-
+  document.querySelector("#courses-list").innerHTML = "";
+  for (const course of filteredCourses) {
+    showCourse(course);
+  }
 }
 
 function showCourse(course) {
-
-    const html = /* html */ `
+  const html = /* html */ `
     <br>
     <li>${course.name}</li>
     <li>${course.ectsPoints}</li>
-    
-    `
-
-    document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
-   
+    `;
+  document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
 }
 
-// 3. Lav en funktion, der filtrer listen af `courses` på baggrund af valgte `option` i `select` (se `environment-05`). 
+// 3. Lav en funktion, der filtrer listen af `courses` på baggrund af valgte `option` i `select` (se `environment-05`).
 // Filtreringen ændrer sig hver gang en ny `option` vælges.
 
 function filterByEcts(event) {
+  const selected = Number(event.target.value);
 
-    const selected = Number(event.target.value);
-    // if (selected == "5") {
-    //    newCourseList = courses.filter(course => course.ectsPoints == "5")
-    // }
-
-    const newCourseList = courses.filter(course => course.ectsPoints === selected)
-    // eller .toString()
-
-    showCourses(newCourseList);
+  const newCourseList = courses.filter((course) => course.ectsPoints === selected);
+  // eller .toString()
+  showCourses(newCourseList);
 }
-
