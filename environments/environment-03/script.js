39a0f739caa4a315f67cb64b543bbe817dffa173
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-06-11 20:19:56 +0200

    gg

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -6,14 +6,15 @@ window.addEventListener("load", start);
 // Hvert objekt har følgende properties: `name` (string),
 // `price` (number) og `inStock` (boolean).
 const productList = [
-  { name: "Taske", price: 444, inStock: true },
-  { name: "Sko", price: 33, inStock: false },
-  { name: "Ur", price: 2034, inStock: true }
+  { name: "Sko", price: 444, inStock: true },
+  { name: "Ur", price: 33, inStock: true },
+  { name: "Taske", price: 2034, inStock: false }
 ];
 
 function start() {
   console.log("hello test 8");
 
+//   sortBySelected(); - virker ikke
   showProducts();
 }
 
@@ -22,6 +23,8 @@ function start() {
 
 function showProducts() {
   console.log("show loop");
+  document.querySelector("#list-container").innerHTML = "";
+//   sortBySelected(); - virker ikke
 
   for (const product of productList) {
     showProduct(product);
@@ -32,11 +35,30 @@ function showProduct(product) {
   console.log("show them");
 
   const html = /*html*/ `
-    <p>Product: ${product.name}, ${product.price}kr</p>
+    <p>Product: ${product.name}, ${product.price} kr</p>
     `;
 
   document.querySelector("#list-container").insertAdjacentHTML("beforeend", html);
+
+  document.querySelector("#select-sort-by").addEventListener("change", sortBySelected);
 }
 
 // 3. Lav en funktion der kan sortere listen efter
-// `name`, `price` og `inStock`. Sorteringen skal ske på baggrund af valgte `option` i `select` (se `environment-03`). Sortering ændrer sig hver gang en ny sortering `option` vælges.
+// `name`, `price` og `inStock`. Sorteringen skal ske på baggrund af valgte `option` i `select` 
+// (se `environment-03`). Sortering ændrer sig hver gang en ny sortering `option` vælges.
+
+function sortBySelected(event) {
+    console.log(event.target.value);
+
+    const selected = event.target.value;
+
+    if (selected === "name" ) {
+        console.log("hihi", selected);
+
+        productList.sort((productA, productB) => productA[selected].localeCompare(productB[selected]))
+    } else if (selected === "price" || selected === "inStock") {
+      productList.sort((productA, productB) => productA[selected] - productB[selected]);
+    }
+    showProducts();
+
+}
